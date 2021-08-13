9ae0b0452c6ce78a0abb4a4b4a7f0891e3a4f0cf
wilgur513 <wilgur513@naver.com>
2021-08-13 21:27:32 +0900

    project create

---
+++ src/main/java/com/wilgur513/inflearnrestapi/InflearnRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.wilgur513.inflearnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wilgur513/inflearnrestapi/InflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilgur513.inflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
