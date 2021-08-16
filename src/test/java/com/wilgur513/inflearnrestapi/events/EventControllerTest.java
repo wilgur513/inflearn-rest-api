51231281a1e420fccd4cdd9eab48a8d33564a97d
wilgur513 <wilgur513@naver.com>
2021-08-17 01:38:16 +0900

    add mock to test database
    - mocking EventRepository

--- src/test/java/com/wilgur513/inflearnrestapi/events/EventControllerTest.java
+++ src/test/java/com/wilgur513/inflearnrestapi/events/EventControllerTest.java
@@ -1,10 +1,12 @@
 package com.wilgur513.inflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,6 +14,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -25,6 +29,9 @@ public class EventControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
@@ -40,6 +47,8 @@ public class EventControllerTest {
                 .limitOfEnrollment(100)
                 .location("강남역 D2")
                 .build();
+        event.setId(10);
+        when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events")
                     .contentType(MediaType.APPLICATION_JSON)
