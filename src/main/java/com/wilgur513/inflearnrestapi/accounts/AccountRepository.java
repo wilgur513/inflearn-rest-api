a36eaeaddf47d3855e1624dff917b149a638072d
wilgur513 <wilgur513@naver.com>
2021-08-26 13:51:32 +0900

    implements AccountService and AccountRepository

---
+++ src/main/java/com/wilgur513/inflearnrestapi/accounts/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.wilgur513.inflearnrestapi.accounts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByEmail(String email);
+}
---
+++ src/main/java/com/wilgur513/inflearnrestapi/accounts/AccountService.java
@@ -0,0 +1,32 @@
+package com.wilgur513.inflearnrestapi.accounts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService implements UserDetailsService {
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream().map(r -> new SimpleGrantedAuthority("ROLE_" + r.name())).collect(Collectors.toSet());
+    }
+}
